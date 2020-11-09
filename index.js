f7a88c82342cab53e13af04dfee87892d2f95524
ahonore42 <a1honore80@gmail.com>
2020-11-09 16:19:33 -0500

    add solution

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-var express = require('express');
-var ejsLayouts = require('express-ejs-layouts');
-var app = express();
+require('dotenv').config()
+let express = require('express');
+let ejsLayouts = require('express-ejs-layouts');
+let app = express();
+let multer = require('multer')
+let upload = multer({ dest: './uploads/'})
+let cloudinary = require('cloudinary')
+let imgUrl = cloudinary.url('i5gehdy7vlsmx20ug32x', {width: 250, height: 250})
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+cloudinary.config(process.env.CLOUDINARY_URL)
+
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', { image: imgUrl });
 });
+
+app.post('/', upload.single('myFile'), (req, res )=> {
+  cloudinary.uploader.upload(req.file.path, (result) => {
+    res.send(result.url)
+  })
+  
+})
+
 
 app.listen(3000);
